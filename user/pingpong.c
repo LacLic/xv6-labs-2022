ce121e1e4486172edd79b93f028545c2e84a0e3b
root <root@LacLic-S-LaPToP.localdomain>
2023-07-26 21:11:56 +0800

    remove listen()

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,15 +1,6 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int listen(int fd, char *buf, char size) {
-  while(1) {
-    int n = read(fd, buf, size); // Read n bytes into buf; returns number read; or 0 if end of file.
-    if(n > 0) return n;
-  }
-  
-  return -1;
-}
-
 int
 main(int argc, char *argv[])
 {
@@ -25,10 +16,10 @@ main(int argc, char *argv[])
 
   if(son_pid > 0){
     write(p[1], "ping", 4); // Write n bytes from buf to file descriptor fd; returns n.
-    listen(p[0], buf, sizeof(buf));
+    read(p[0], buf, sizeof(buf));
     fprintf(1, "%d: received %s\n", pid, buf);
   }else{
-    listen(p[0], buf, sizeof(buf));
+    read(p[0], buf, sizeof(buf));
     fprintf(1, "%d: received %s\n", pid, buf);
     write(p[1], "pong", 4);
   }
